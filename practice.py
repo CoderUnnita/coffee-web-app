6d5d5f1a1dae1a594b985ac694e25a794ca2ccfb
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-11-07 08:54:55 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,35 @@
+a=12
+cube=a*a*a
+print ("The Cube ",cube)
+''' This is my multi line comment'''
+
+# Calculator code
+num1=float(input("Enter Your First Number:"))
+num2=float(input("Enter Your Second Number:"))
+
+print("Select The Action You Want To Perform :-")
+print("Addiction - press 1")
+print("Subtraction - press 2")
+print("Division - press 3")
+print("Multiplication - press 4")
+
+opert= int(input("Enter Your Operation No:"))
+
+if opert==1:
+    add=num1+num2
+    print ("Addition :",add)
+
+elif opert==2:
+    sub=num1-num2
+    print("Subtraction :",sub)
+
+elif opert==3:
+    did=num1/num2
+    print("Division :",did)
+
+elif opert==4:
+    mul=num1*num2
+    print("Multiplication :",mul)   
+
+else:
+    print("Invalid Number , Can't Perform Any Opration :)")
